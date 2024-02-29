03229d4b656a506fa6415b8bc62aa868164f66cf
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-02-29 19:51:35 +0530

    update .env

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
-from sectors import sectors_url
+import os
 
+load_dotenv()
 
 app = Flask(__name__)
 
-
-
+domain = os.environ.get('domain')
+sectors_url = {
+    'pharma' : f'pharmaceuticals.html',
+    'chem' : f'{domain}/chemicals.html',
+    'cs' : f'{domain}/computers-software.html',
+    'fert' : f'{domain}/fertilisers.html',
+    'petrochem' : f'https://www.moneycontrol.com/stocks/sectors/petrochemicals.html',
+    'trading' : f'https://www.moneycontrol.com/stocks/sectors/trading.html',
+    'cement' : [f'{domain}/cement-major.html', f'{domain}/cement-mini.html'],
+    'banks' : [f'{domain}/banks-private-sector.html', f'{domain}/banks-public-sector.html'],
+    'finance' : [f'{domain}/finance-general.html',f'{domain}/finance-housing.html',f'{domain}/finance-investments.html',f'{domain}/finance-leasing-hire-purchase.html',f'{domain}/finance-term-lending-institutions.html'],
+    'steel' : [f'{domain}/steel-large.html', f'{domain}/steel-medium-small.html'],
+    'misc' : f'{domain}/miscellaneous.html'
+}
 
 def stock_data_to_dict(url):
     data = requests.get(url)
---
+++ temp.py
@@ -0,0 +1,7 @@
+domain = 'http://www.moneycontrol.com/stocks/sectors'
+
+
+
+
+# domain = f'http:///{sectors_url['pharma']}'
+print(domain)
