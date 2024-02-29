b348459e21282edd5dc3fcf4d3580b6e1ecaf18a
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-02-29 20:04:08 +0530

    update main

--- main.py
+++ main.py
@@ -9,20 +9,6 @@ import os
 
 app = Flask(__name__)
 
-domain = 'http://www.moneycontrol.com/stocks/sectors'
-sectors_url = {
-    'pharma' : f'pharmaceuticals.html',
-    'chem' : f'{domain}/chemicals.html',
-    'cs' : f'{domain}/computers-software.html',
-    'fert' : f'{domain}/fertilisers.html',
-    'petrochem' : f'{domain}/petrochemicals.html',
-    'trading' : f'{domain}/trading.html',
-    'cement' : [f'{domain}/cement-major.html', f'{domain}/cement-mini.html'],
-    'banks' : [f'{domain}/banks-private-sector.html', f'{domain}/banks-public-sector.html'],
-    'finance' : [f'{domain}/finance-general.html',f'{domain}/finance-housing.html',f'{domain}/finance-investments.html',f'{domain}/finance-leasing-hire-purchase.html',f'{domain}/finance-term-lending-institutions.html'],
-    'steel' : [f'{domain}/steel-large.html', f'{domain}/steel-medium-small.html'],
-    'misc' : f'{domain}/miscellaneous.html'
-}
 
 def stock_data_to_dict(url):
     data = requests.get(url)
